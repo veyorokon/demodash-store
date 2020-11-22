df258ae1b0e87da40fe2d4ebf0d6a1f850a5a6a0
Vahid <veyorokon@gmail.com>
2020-11-22 14:08:19 -0500

    Added config for api

--- react/src/api.js
+++ react/src/api.js
@@ -2,12 +2,16 @@ import {ApolloClient} from "apollo-client";
 import {InMemoryCache} from "apollo-cache-inmemory";
 import {HttpLink} from "apollo-link-http";
 
-export const API_SERVER = "http://192.168.200.238:8000/";
-export const API_MEDIA = "http://192.168.200.238:8000/media/";
+export const API_SERVER =
+  process.env.REACT_APP_API_SERVER || "http://192.168.200.238:8000";
+
+export const API_MEDIA = `${API_SERVER}/media/`;
+
+export const DASHBOARD_HOST = process.env.REACT_APP_DASHBOARD_HOST || "";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: `${API_SERVER}graphql/`
+  uri: `${API_SERVER}/graphql/`
 });
 
 export const client = new ApolloClient({
