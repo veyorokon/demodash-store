67332e36238cca48c6f2fd1e5c705e8690c630fe
Vahid <veyorokon@gmail.com>
2020-11-26 13:14:22 -0500

    Added configs for production

--- react/src/api.js
+++ react/src/api.js
@@ -3,11 +3,15 @@ import {InMemoryCache} from "apollo-cache-inmemory";
 import {HttpLink} from "apollo-link-http";
 
 export const API_SERVER =
-  process.env.REACT_APP_API_SERVER || "http://192.168.200.238:8000";
+  `${process.env.REACT_APP_API_HOST}` || "http://192.168.200.238:8000";
 
-export const API_MEDIA = `${API_SERVER}/media/`;
+export const API_MEDIA =
+  `${process.env.REACT_APP_API_MEDIA}/${process.env.REACT_APP_API_STATIC_BUCKET}/media/` ||
+  API_SERVER.concat("/media/");
 
-export const DASHBOARD_HOST = process.env.REACT_APP_DASHBOARD_HOST || "";
+export const RECAPTCHA_PUBLIC_KEY =
+  process.env.REACT_APP_RECAPTCHA_PUBLIC_KEY ||
+  "6Left-cZAAAAAEy5TuCT7AGnJ9YPmYqGpAbKAjJH";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
